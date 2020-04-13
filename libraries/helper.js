279666ed25b6a701a98fd5f0896e156172abc79a
justinsulik <justin.sulik@gmail.com>
2020-04-13 13:21:12 +0200

    more text

--- libraries/helper.js
+++ libraries/helper.js
@@ -17,7 +17,6 @@ function prepareData(experiment_start_time){
   data.responses = jsPsych.data.get().json();
   data.trial_id = trial_id;
   dataJSON = JSON.stringify(data);
-
   return dataJSON;
 }
 
